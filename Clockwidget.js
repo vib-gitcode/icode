9db1c802200d4afc6ac5858e9aea26738940f703
Vibnet <vibhav68@outlook.com>
2026-07-06 21:19:02 +0000

    commit

--- Clockwidget.js
+++ Clockwidget.js
@@ -1,1 +1,5 @@
-var 
+function timeWidget (){
+var date = new Date()
+const Widget = document.getElementById("w2")
+Widget.innerHTML = "The time is "+date.toLocaleTimeString()}
+setInterval(timeWidget,100)
