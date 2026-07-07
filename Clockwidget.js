79bc42fb512b19a349ae6b7487c88dfda6b5f37c
Vibnet <vibhav68@outlook.com>
2026-07-07 22:17:01 +0000

    commit

--- Clockwidget.js
+++ Clockwidget.js
@@ -2,4 +2,7 @@ function timeWidget (){
 var date = new Date()
 const Widget = document.getElementById("w2")
 Widget.innerHTML = "The time is "+date.toLocaleTimeString()}
+hour: '2-digit';
+minute: '2-digit';
+second: '0-digit';
 setInterval(timeWidget,100)
