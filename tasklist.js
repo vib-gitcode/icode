ea9926e96aa0907a44788d1eb9f2c0d04d58993a
Vibnet <vibhav68@outlook.com>
2026-07-07 23:15:55 +0000

    make task list widget

--- tasklist.js
+++ tasklist.js
@@ -1,3 +1,12 @@
-function taskWidget(){
 var taskWidget = document.getElementById("w3")
+function newtask(){
+const input = document.getElementById("tasktextbox")
+const tasklist = document.getElementById("tasks")
+tasklist.innerHTML+="<li>"+input.value+"</li>"
+
+}
+function delAllTasks() {
+    const tasklist = document.getElementById("tasks")
+tasklist.innerHTML=" "
+
 }
